211451110545f0d18a86bb43e3ec623e2856f353
Wei Yuan <475214939@qq.com>
2018-11-13 16:53:13 +0800

    add config

---
+++ config/index.js
@@ -0,0 +1,2 @@
+const ZENTAO_URL = 'http://192.168.71.212';
+const CONFLUENCE_URL = 'http://192.168.29.218:8888';
--- js/index.js
+++ js/index.js
@@ -1,39 +1,50 @@
-function b() {
-  console.log('222222');
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://192.168.29.218:8888/rest/popular/1/stream/content?days=7&pageSize=20", true);
+let confluenceContent = '';
+let zenTaoContent = '';
+
+function fetch(url, func) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
   xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var resp = JSON.parse(xhr.responseText);
-      console.log('dddd222::', resp);
-    } else {
-      console.log('over::');
-    }
+    if (xhr.readyState == 4) func(xhr);
   }
   xhr.send();
 }
 
-b();
-
-function getContent() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://192.168.71.212/zentaopms/www/my/", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      console.log('uuu', xhr.responseText);
-      document.getElementById('content').innerText = xhr.responseText;
+function getConfluenceContent() {
+  fetch(
+    `${CONFLUENCE_URL}/rest/popular/1/stream/content?days=7&pageSize=20`,
+    (xhr) => {
+      const resp = JSON.parse(xhr.responseText);
+      console.log('con::', resp);
     }
-  }
-  xhr.send();
-  
-  xhr.open("GET", "http://192.168.71.212/zentaopms/www/block-printBlock-977-my.html", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      console.log('uuu2222', xhr.responseText);
-      document.getElementById('content').innerText = xhr.responseText;
-    }
-  }
-  xhr.send();
+  );
 }
 
-getContent();
+function getZenTaoContent() {
+  fetch(
+    `${ZENTAO_URL}/zentaopms/www/my/`,
+    (xhr) => {
+      const container = document.createElement('div');
+      container.innerHTML = xhr.responseText;
+      const bug = container.querySelector("[data-name='指派给我的Bug']").getAttribute('data-url');
+      const work = container.querySelector("[data-name='指派给我的任务']").getAttribute('data-url');
+    
+      fetch(
+        `http://192.168.71.212${bug}`,
+        (xhr) => {
+          console.log('bug', bug, xhr.responseText);
+        }
+      );
+    
+      fetch(
+        `http://192.168.71.212${work}`,
+        (xhr) => {
+          console.log('work', work, xhr.responseText);
+        }
+      );
+    }
+  );
+}
+
+getConfluenceContent();
+getZenTaoContent();
