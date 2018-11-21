68f4c6f4f0d5806dc9269e9a73a20b6fb150a354
Wei Yuan <475214939@qq.com>
2018-11-21 11:51:22 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -1,2 +1,369 @@
 const ZENTAO_URL = 'http://192.168.71.212';
 const CONFLUENCE_URL = 'http://192.168.29.218:8888';
+const GITEE_URL = 'https://gitee.com';
+
+// swagger
+const SWAGGER_URLS = [
+  {
+    name: '活动服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/activity-service/swagger-ui.html#/'
+  },
+  {
+    name: '积分服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/point-service/swagger-ui.html#/',
+  },
+  {
+    name: '店铺服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/store-service/swagger-ui.html#/',
+  },
+  {
+    name: '优惠券服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/coupon-service/swagger-ui.html#/',
+  },
+  {
+    name: '购物车服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/cart-service/swagger-ui.html#/',
+  },
+  {
+    name: '订单提交接口',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/ofc-service/swagger-ui.html#/',
+  },
+  {
+    name: '支付服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/financial-service/swagger-ui.html#/',
+  },
+  {
+    name: '用户服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/user-service/swagger-ui.html#/',
+  },
+  {
+    name: '商家优惠服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/discount-service/swagger-ui.html#/',
+  },
+  {
+    name: '积分商品服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/pointshop-service/swagger-ui.html#/',
+  },
+  {
+    name: '广告服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/advertisement-service/swagger-ui.html#/',
+  },
+  {
+    name: '发票服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/invoice-service/swagger-ui.html#/',
+  },
+  {
+    name: '商品服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/product-service/product/swagger-ui.html#/',
+  },
+  {
+    name: '售前服务',
+    img: 'img/swagger.png',
+    url: 'http://test-api.intra.casstime.com/presale-service/swagger-ui.html#/',
+  },
+  {
+    name: '询价服务',
+    img: 'img/swagger.png',
+    url: 'http://192.168.29.218:8888/pages/viewpage.action?pageId=1998850',
+  }
+];
+
+// tapd wiki
+const TAPD_URLS = {
+  name: '电商平台知识库',
+  img: 'img/tapd.png',
+  url: 'https://www.tapd.cn/37925682/markdown_wikis/#1137925682001000099'
+};
+
+// aliyun
+const ALIYUN_URLS = {
+  name: '日志查看（用户名：logger, 密码：Casstime88*）',
+  img: 'img/aliyun.png',
+  url: 'https://sls.console.aliyun.com/?spm=5176.10560872.1.1.105234c0y4r26I#/',
+}
+
+// 测试环境
+const TEST_URLS = [
+  {
+    name: 'PC电商平台',
+    img: 'img/test.png',
+    url: 'https://ec-test.casstime.com/market/portal'
+  },
+  {
+    name: '卖家中心',
+    img: 'img/test.png',
+    url: 'https://ec-test.casstime.com/sellerMgr/index'
+  },
+  {
+    name: '管理中心',
+    img: 'img/test.png',
+    url: 'https://ec-test.casstime.com/systemMgr/admin'
+  },
+  {
+    name: '小马客户端下载',
+    img: 'img/test.png',
+    url: 'http://cassnotify.oss-cn-shenzhen.aliyuncs.com/cassim-test/release-nw/%E5%BC%80%E6%80%9D%E5%B0%8F%E9%A9%ACSetup.exe'
+  }
+];
+
+// 演示环境
+const DEMO_URLS = [
+  {
+    name: 'PC电商平台',
+    img: 'img/test.png',
+    url: 'https://ec-demo.casstime.com/market/portal'
+  },
+  {
+    name: '卖家中心',
+    img: 'img/test.png',
+    url: 'https://ec-demo.casstime.com/sellerMgr/index'
+  },
+  {
+    name: '管理中心',
+    img: 'img/test.png',
+    url: 'https://ec-demo.casstime.com/systemMgr/admin'
+  },
+  {
+    name: '小马客户端下载',
+    img: 'img/test.png',
+    url: 'http://cassnotify.oss-cn-shenzhen.aliyuncs.com/cassim-demo/release-nw/%E5%BC%80%E6%80%9D%E5%B0%8F%E9%A9%ACSetup.exe'
+  }
+];
+
+// 组件库
+const COMPONENT_URLS = {
+  name: 'bricks组件库',
+  img: 'img/test.png',
+  url: 'https://gitee.com/cassfrontend/bricks',
+};
+
+// 前端社区
+const COMMUNITY = [
+  {
+    name: 'Github',
+    img: 'img/github.png',
+    url: 'https://github.com/'
+  },
+  {
+    name: 'Stack Overflow',
+    img: 'img/stackoverflow.png',
+    url: 'https://stackoverflow.com/'
+  },
+  {
+    name: '掘金',
+    img: 'img/juejin.png',
+    url: 'https://juejin.im/'
+  },
+  {
+    name: 'CSDN',
+    img: 'img/csdn.png',
+    url: 'https://www.csdn.net/'
+  },
+  {
+    name: 'CNode',
+    img: 'img/cnode.png',
+    url: 'https://cnodejs.org/'
+  }
+];
+
+// 框架类库
+const FRAME = [
+  {
+    name: 'React Native',
+    img: 'img/reactnative.png',
+    url: 'https://reactnative.cn/'
+  },
+  {
+    name: 'React',
+    img: 'img/react.png',
+    url: 'https://reactjs.org/'
+  },
+  {
+    name: 'Vue',
+    img: 'img/vue.png',
+    url: 'https://cn.vuejs.org/'
+  },
+  {
+    name: 'Jquery',
+    img: 'img/jquery.png',
+    url: 'https://jquery.com/'
+  },
+  {
+    name: 'Bootstrap',
+    img: 'img/bootstrap.png',
+    url: 'http://www.bootcss.com/'
+  },
+];
+
+// 状态管理
+const STATE = [
+  {
+    name: 'Redux',
+    img: 'img/redux.svg',
+    url: 'https://cn.redux.js.org/'
+  },
+  {
+    name: 'Mobx',
+    img: 'img/mobx.png',
+    url: 'https://cn.mobx.js.org/'
+  },
+  {
+    name: 'Vuex',
+    img: 'img/vue.png',
+    url: 'https://vuex.vuejs.org/zh/guide/'
+  }
+];
+
+// 第三方类库
+const OTHER_FRAME = [
+  {
+    name: 'Ant Design',
+    img: 'img/antdesign.svg',
+    url: 'https://ant.design/'
+  },
+  {
+    name: 'Element',
+    img: 'img/element.svg',
+    url: 'http://element-cn.eleme.io/#/'
+  },
+  {
+    name: 'Redux Saga',
+    img: 'img/redux.svg',
+    url: 'https://redux-saga-in-chinese.js.org/docs/introduction/BeginnerTutorial.html'
+  },
+  {
+    name: 'Lodash',
+    img: 'img/lodash.png',
+    url: 'https://www.lodashjs.com/'
+  }
+];
+
+// 构建工具
+const BUILD = [
+  {
+    name: 'Webpack',
+    img: 'img/webpack.svg',
+    url: 'https://www.webpackjs.com/'
+  },
+  {
+    name: 'Gulp',
+    img: 'img/gulp.svg',
+    url: 'https://www.gulpjs.com.cn/'
+  },
+  {
+    name: 'Babel',
+    img: 'img/babel.png',
+    url: 'https://www.babeljs.cn/'
+  },
+  {
+    name: 'Typescript',
+    img: 'img/typescript.png',
+    url: 'https://zhongsp.gitbooks.io/typescript-handbook/'
+  }
+];
+
+// 学习手册
+const STUDY_BOOK = [
+  {
+    name: 'React 小书',
+    img: 'img/react.png',
+    url: 'http://huziketang.mangojuice.top/books/react/'
+  },
+  {
+    name: 'Typescript 入门教程',
+    img: 'img/typescript.png',
+    url: 'https://ts.xcatliu.com/'
+  },
+  {
+    name: 'flex 布局',
+    img: '',
+    url: 'http://www.ruanyifeng.com/blog/2015/07/flex-grammar.html'
+  }
+];
+
+// CSS相关
+const CSS_TOOLS = [
+  {
+    name: 'Sass',
+    img: 'img/sass.svg',
+    url: 'https://sass-guidelin.es/zh/'
+  },
+  {
+    name: 'Less',
+    img: 'img/less.svg',
+    url: 'http://lesscss.cn/'
+  }
+];
+
+// node 相关
+const NODE = [
+  {
+    name: 'express',
+    img: 'img/express.png',
+    url: 'http://www.expressjs.com.cn/'
+  },
+  {
+    name: 'hapi',
+    img: 'img/hapi.png',
+    url: 'https://hapijs.com/'
+  },
+];
+
+// 字体图标
+const FONT = [
+  {
+    name: '阿里图标',
+    img: 'img/alitubiao.png',
+    url: 'http://iconfont.cn/'
+  }
+];
+
+// IDE
+const IDE = [
+  {
+    name: 'Webstorm',
+    img: 'img/webstorm.jpg',
+    url: 'https://www.jetbrains.com/webstorm/'
+  },
+  {
+    name: 'VS Code',
+    img: 'img/vscode.png',
+    url: 'https://code.visualstudio.com/'
+  }
+];
+
+// 在线工具
+const ONLINE_TOOL = [
+  {
+    name: 'Can I Use',
+    img: 'img/caniuse.png',
+    url: 'https://caniuse.com/'
+  }
+];
+
+// 客户端开发
+const CLIENT = [
+  {
+    name: 'NW.js',
+    img: 'img/nw.png',
+    url: 'https://nwjs.org.cn/'
+  },
+  {
+    name: 'Electron',
+    img: 'img/electron.jpg',
+    url: 'https://electronjs.org/'
+  },
+];
--- js/index.js
+++ js/index.js
@@ -12,39 +12,292 @@ function fetch(url, func) {
 
 function getConfluenceContent() {
   fetch(
-    `${CONFLUENCE_URL}/rest/popular/1/stream/content?days=7&pageSize=20`,
+    `${CONFLUENCE_URL}/rest/popular/1/stream/content?days=7&pageSize=60`,
     (xhr) => {
-      const resp = JSON.parse(xhr.responseText);
-      console.log('con::', resp);
+      try {
+        const resp = JSON.parse(xhr.responseText);
+        const result = resp.streamItems.map(item => {
+          return `
+            <div class="confluence-item">
+              <div class="confluence-zan">
+                <p>^</p>
+                <p>${item.numberOfLikes}</p>
+              </div>
+              <div class="confluence-right">
+                <p>
+                  <a href="${CONFLUENCE_URL}${item.url}" target="_blank">${item.title}</a>
+                </p>
+                <div class="confluence-right-p">
+                  <div>${item.friendlyDate}</div>
+                  <div class="author">
+                    <img class="avator" src="${CONFLUENCE_URL}${item.author.avatarUrl}"/>
+                    <a href="${CONFLUENCE_URL}${item.author.url}" target="_blank">${item.author.fullName}</a>
+                  </div>
+                </div>
+              </div>          
+            </div>
+          `
+        });
+        const container = document.createElement('div');    
+        for (let i = 0; i < result.length; i++) {
+          container.innerHTML += result[i];
+        }
+        document.querySelector('.confluence').innerHTML = container.innerHTML;
+      } catch (e) {
+        const errerDiv = `
+          <div class="error-go-login">
+            <a href="${CONFLUENCE_URL}/dashboard.action" class="login-button">去登陆</a>
+          </div>
+        `;
+        document.querySelector('.confluence').innerHTML = errerDiv;
+      } 
     }
   );
+}
+
+function getConfluenceNewContent() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", `${CONFLUENCE_URL}/rest/dashboardmacros/1.0/updates`, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      const resp = JSON.parse(xhr.responseText);
+      const result = [];
+      for (let i = 0; i < resp.changeSets.length; i++) {
+        for (let j = 0; j < resp.changeSets[i].recentUpdates.length; j++) {
+          const item = resp.changeSets[i].recentUpdates[j];
+          result.push(`
+            <div class="confluence-item">
+              <div class="confluence-zan">
+                <p>^</p>
+                <p>${item.numberOfLikes || 0}</p>
+              </div>
+              <div class="confluence-right">
+                <p>
+                  <a href="${CONFLUENCE_URL}${item.urlPath}" target="_blank">${item.title}</a>
+                </p>
+                <div class="confluence-right-p">
+                  <div>${item.friendlyUpdateTime}</div>
+                  <div class="author">
+                    <img class="avator" src="${CONFLUENCE_URL}${resp.changeSets[i].profilePictureInfo.downloadPath}"/>
+                    <a href="javascript:;" target="_blank">${resp.changeSets[i].modifier.fullName}</a>
+                  </div>
+                </div>
+              </div>          
+            </div>
+          `);
+        }
+      }
+      const container = document.createElement('div');    
+      for (let i = 0; i < result.length; i++) {
+        container.innerHTML += result[i];
+      }
+      document.querySelector('.confluence').innerHTML = container.innerHTML;
+    }
+  }
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
+  const params = "maxResults=20&tab=all&showProfilePic=true&labels=&spaces=&users=&types=&category=&spaceKey=&";
+  xhr.send(params);  
 }
 
 function getZenTaoContent() {
   fetch(
     `${ZENTAO_URL}/zentaopms/www/my/`,
     (xhr) => {
-      const container = document.createElement('div');
-      container.innerHTML = xhr.responseText;
-      const bug = container.querySelector("[data-name='指派给我的Bug']").getAttribute('data-url');
-      const work = container.querySelector("[data-name='指派给我的任务']").getAttribute('data-url');
-    
-      fetch(
-        `${ZENTAO_URL}${bug}`,
-        (xhr) => {
-          console.log('bug', bug, xhr.responseText);
-        }
-      );
-    
-      fetch(
-        `${ZENTAO_URL}${work}`,
-        (xhr) => {
-          console.log('work', work, xhr.responseText);
-        }
-      );
+      try {
+        const container = document.createElement('div');
+        container.innerHTML = xhr.responseText;
+        const bug = container.querySelector("[data-name='指派给我的Bug']").getAttribute('data-url');
+        const work = container.querySelector("[data-name='最新动态']").getAttribute('data-url');
+      
+        fetch(
+          `${ZENTAO_URL}${bug}`,
+          (xhr) => {
+            document.querySelector('.zentao-bug').innerHTML = xhr.responseText;
+            document.querySelector('.zentao-bug')
+          }
+        );
+      
+        fetch(
+          `${ZENTAO_URL}${work}`,
+          (xhr) => {
+            document.querySelector('.zentao-work').innerHTML = xhr.responseText;
+          }
+        );
+      } catch (e) {
+        const errerDiv = `
+          <div class="error-go-login">
+            <a href="${ZENTAO_URL}/zentaopms/www/my" class="login-button">去登陆</a>
+          </div>
+        `;
+        document.querySelector('.zentao-bug').innerHTML = errerDiv;
+        document.querySelector('.zentao-work').innerHTML = errerDiv;
+      }
     }
   );
 }
 
-getConfluenceContent();
-getZenTaoContent();
+function getGiteeContent() {
+  fetch(
+    `${GITEE_URL}/api/v3/internal/my_resources`,
+    (xhr) => {
+      const { projects_path, projects_count } = JSON.parse(xhr.responseText);
+      const page = Math.ceil(projects_count / 10);
+      for (let i = 0; i < page; i++) {
+        fetch(
+          `${GITEE_URL}/${projects_path}?page=${i+1}`,
+          (xhr) => {
+            const container = document.createElement('div');
+            container.innerHTML = xhr.responseText;
+            const items = container.querySelector('#search-projects-ulist').querySelectorAll('.project')
+            const result = Array.from(items).map(item => {
+              const createTime = item.querySelector('.create-time').innerText.split(/^\r|\r\n|\n|\s$/);
+              return `
+                <div class="gitee-item">
+                  <div class="gitee-item-left">
+                    <a href="${GITEE_URL}${item.querySelector('.author').getAttribute('href')}" target="_blank">
+                      ${item.querySelector('.author').getAttribute('title')}
+                    </a>
+                    /
+                    <a href="${GITEE_URL}${item.querySelector('.repository').getAttribute('href')}" target="_blank">
+                      ${item.querySelector('.repository').getAttribute('title')}
+                    </a>
+                  </div>
+                  <div class="gitee-item-right">
+                    <div>
+                      ${item.querySelector('.description').innerText}
+                    </div>
+                    <div clss="">
+                      ${createTime[1]} ${createTime[2]}
+                    </div>
+                  </div>
+                </div>
+              `
+            });  
+            for (let j = 0; j < result.length; j++) {
+              document.querySelector('.gitee').innerHTML += result[j];
+            }
+          }
+        );
+      }
+      if (!page) {
+        const errerDiv = `
+          <div class="error-go-login">
+            <a href="${GITEE_URL}/login" class="login-button">去登陆</a>
+          </div>
+        `;
+        document.querySelector('.gitee').innerHTML = errerDiv;
+      }
+    }
+  );
+}
+
+function getTwoContent() {
+  getContent('swagger', SWAGGER_URLS);
+  getContent('tapd', TAPD_URLS);
+  getContent('test', TEST_URLS);
+  getContent('demo', DEMO_URLS);
+  getContent('aliyun', ALIYUN_URLS);
+  getContent('component', COMPONENT_URLS);
+}
+
+function getThreeContent() {
+  getContent('floor_1', COMMUNITY);
+  getContent('floor_2', FRAME);
+  getContent('floor_3', STATE);
+  getContent('floor_4', OTHER_FRAME);
+  getContent('floor_5', BUILD);
+  getContent('floor_6', CLIENT);
+  getContent('floor_7', STUDY_BOOK);
+  getContent('floor_8', CSS_TOOLS);
+  getContent('floor_9', NODE);
+  getContent('floor_10', FONT);
+  getContent('floor_11', IDE);
+  getContent('floor_12', ONLINE_TOOL);
+}
+
+/**
+ * @param {string} id
+ * @param {array or object} item 
+ */
+function getContent(id, obj) {
+  if (obj instanceof Array) {
+    const result = obj.map(item => {
+      return `
+        <a href="${item.url}" target="_blank">
+          <div class="content-cards-item">
+            <img src="${item.img}"/>
+            ${item.name}
+          </div>
+        </a>
+      `
+    });
+    for (let j = 0; j < result.length; j++) {
+      document.querySelector(`#${id}`).innerHTML += result[j];
+    }
+    return;
+  }
+
+  document.querySelector(`#${id}`).innerHTML = `
+    <a href="${obj.url}" target="_blank">
+      <div class="content-cards-item">
+        <img src="${obj.img}"/>
+        ${obj.name}
+      </div>
+    </a>
+  `;
+}
+
+function init() {
+  getConfluenceContent();
+  getZenTaoContent();
+  getGiteeContent();
+  getTwoContent();
+  getThreeContent();
+  // setTimeout(() => console.clear(), 2000);
+}
+
+init();
+
+$('.zentao-content').on('click', 'tr', function() {
+  const url = ZENTAO_URL + $(this)[0].getAttribute('data-url');
+  if (!$(this)[0].getAttribute('data-url')) {
+    return;
+  }
+  window.open(url);
+})
+
+$('.zentao-content').on('click', 'a', function(e) {
+  if ($(this).hasClass("login-button")) {
+    return
+  }
+  e.preventDefault();
+  const url = ZENTAO_URL + e.target.getAttribute('href');
+  window.open(url);
+})
+
+$('.nav').on('click', '.nav-button', function() {
+  $('.nav-button').css('color', '#646c7b');
+  $(this).css('color', '#eb3136');
+
+  const containerId = '#container-' + $(this)[0].getAttribute('id').split('-')[1];
+  $('.container').css('display', 'none');
+  $(containerId).css('display', 'flex');
+})
+
+$('.confluence-content').on('click', '.confluence-button', function() {
+  $('.confluence-button').css('color', '#646c7b');
+  $(this).css('color', '#eb3136');
+  if ($(this).attr('id') === 'confluence-header-button-1') {
+    getConfluenceContent();
+  } else {
+    getConfluenceNewContent();
+  }
+})
+
+$('header').on('click', '.switch', function() {
+  if ($(this).attr('class').includes('on')) {
+    $(this).removeClass('on');
+  } else {
+    $(this).addClass('on');
+  }
+});
