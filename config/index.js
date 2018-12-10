4c13c7c0453ec53f3403f2119f65caf499d13fba
Wei Yuan <475214939@qq.com>
2018-12-10 15:37:50 +0800

    优化菜单配置化

--- config/index.js
+++ config/index.js
@@ -103,7 +103,7 @@ const TWO_PAGE_MENU = [
     title: 'aliyun',
     name: '阿里云',
     urls: {
-      name: '日志查看（用户名：logger, 密码：Casstime88*）',
+      name: '日志查看',
       img: 'img/aliyun.png',
       url: 'https://sls.console.aliyun.com/?spm=5176.10560872.1.1.105234c0y4r26I#/',
     }
